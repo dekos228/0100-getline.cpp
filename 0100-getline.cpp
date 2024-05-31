d570dc12b67709545ce2fdfbcc6449d43ef406eb
dekos228 <dekos589191@gmail.com>
2024-05-31 20:27:23 +0200

    Create 0100-getline.cpp

---
+++ 0100-getline.cpp
@@ -0,0 +1,15 @@
+﻿// getline.cpp
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+  using namespace std; // Искать имена в std.
+
+  string user_name = "user"; // Определить переменную.
+  cout << "Login: ";
+  getline(cin, user_name); // Считать строку из потока ввода.
+  cout << "Hello, " << user_name << "!" << endl;
+  return EXIT_SUCCESS; // Возвратим ОС "код успеха".
+}
